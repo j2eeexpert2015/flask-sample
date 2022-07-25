d71fdbc5752b3fcf1b8572b3494285818238cd37
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-07-25 23:31:01 +0530

    Added Parameter

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    parameter = request.args.get('parameter')
     return 'Hello World!'
 
 if __name__ == '__main__':
